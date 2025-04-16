12578a831cd656e88d87c74c63b7daeca85bf564
Naima BELMOKHTAR <naima1503@gmail.com>
2025-04-16 12:17:24 +0300

    alpha_div.R

--- analysis_functions/alpha_div.R
+++ analysis_functions/alpha_div.R
@@ -1,5 +1,5 @@
 # MetaXplore Version 1.0
-# Last modified on 05/03/2024
+# Last modified on 14/04/2025
 # Author: Naima BEL MOKHTAR (Email: naima1503@gmail.com)
 ########################################################
 
@@ -144,131 +144,7 @@ source("tools_functions.R")
     df
   }
 
-#Generate alpha diversity boxplot
-# Modified from alpha diversity in microbiomSeq package (umerijaz/microbiomeSeq)  
-  alpha.div.plot <- function(physeq, method, grouping_column, pValueCutoff=0.05, neword=NULL, changefac=T){
-    #enforce orientation
-    if(taxa_are_rows(physeq)){
-      physeq <- t(physeq)
-    }
-    abund_table <- otu_table(physeq)
-    meta_table <- sample_data(physeq)
-    
-    #get diversity measure using selected methods
-    div.df <- alpha_div(physeq, method)
-    
-    #Exclude Goods coverage from the plot
-    div.df <- subset(div.df, measure!="Goods Coverage")
-  
-    
-    #=add grouping information to alpha diversity measures
-    df<-data.frame(div.df,(meta_table[,grouping_column])[as.character(div.df$sample),])
-    
-    #change the facto's order of the targeted column
-      if (changefac == T){
-      df <- order.levels.df(df = df,factor = grouping_column, orderd_factor1 = neword, changefac = changefac)
-      }
-      
-    #perform anova of diversity measure between groups if the group contain more than 1 element
-    df_pw <-NULL
-    if(length(unique(as.character(meta_table[[grouping_column]])))>1){
-      anova_res <- perform_anova2(df,meta_table,grouping_column,pValueCutoff)
-      df_pw <- data.frame(anova_res$df_pw) #get pairwise p-values
-      #df_pw$measure <- factor(df_pw$measure, levels = c("Richness", "Simpson", "Shannon", "Pielou's evenness", "PD"))
-      }
-      
-    #Order indices
-     #df$measure <- factor(df$measure, levels = c("Richness", "Simpson", "Shannon", "Pielou's evenness", "PD"))
-    
-      #Draw the boxplots
-      p <- ggplot(aes_string(x=grouping_column,y="value",color=grouping_column),data=df)+
-            geom_boxplot()+
-            geom_jitter(position = position_jitter(height = 0, width=0))+
-            theme_bw()+
-            facet_wrap(~ measure, scales="free",nrow=1)+
-            ylab("Observed Values")+
-            theme(strip.background = element_rect(fill = "white"), panel.spacing = unit(0.005, "cm", data = NULL))+
-            xlab("")
-      
-      #This loop will generate the lines and significance
-      if(!is.null(df_pw)){ #this only happens when we have significant pairwise anova results
-        df[,grouping_column] <- as.factor(df[,grouping_column])
-         for(i in 1:dim(df_pw)[1]){
-          p <- p + geom_path(inherit.aes=F,aes(x,y),data = data.frame(x = c(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"])),which(levels(df[,grouping_column])==as.character(df_pw[i,"to"]))), y = c(as.numeric(as.character(df_pw[i,"y"])),as.numeric(as.character(df_pw[i,"y"]))), measure=c(as.character(df_pw[i,"measure"]),as.character(df_pw[i,"measure"]))), color="black",lineend = "butt",arrow = arrow(angle = 90, ends = "both", length = unit(0.1, "inches")))
-          p <- p + geom_text(inherit.aes=F,aes(x=x,y=y,label=label),data=data.frame(x=(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"]))+which(levels(df[,grouping_column])==as.character(df_pw[i,"to"])))/2,y=as.numeric(as.character(df_pw[i,"y"])),measure=as.character(df_pw[i,"measure"]),label=as.character(cut(as.numeric(as.character(df_pw[i,"p"])),breaks=c(-Inf, 0.001, 0.01, 0.05, Inf),label=c("***", "**", "*", "")))))
-          #p<-p + geom_text(inherit.aes=F,aes(x=x,y=y,label=p.value),data=data.frame(x=(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"]))+which(levels(df[,grouping_column])==as.character(df_pw[i,"to"])))/2,y=(as.numeric(as.character(df_pw[i,"y"])))+((as.numeric(as.character(df_pw[i,"y"])))*0.025),measure=as.character(df_pw[i,"measure"]),p.value=as.character(df_pw[i,"p"])))
-          
-        }
-      }
-      return(p)
-  }
-
-#Perform statistical ANOVA test
-# Modified from alpha diversity in microbiomSeq package (umerijaz/microbiomeSeq)  
-  perform_anova2 <- function(df, meta_table, grouping_column, pValueCutoff){
-    temp <- data.frame(df, meta_table[, grouping_column])
-    colnames(temp) <- c(colnames(df), "group")
-    dt<-data.table::data.table(temp)
-    
-    #specifying a p-value cutoff for the ggplot2 strips
-    pval<-dt[, list(pvalue = sprintf("%.2g",tryCatch(summary(aov(value ~ group))[[1]][["Pr(>F)"]][1],error=function(e) NULL))),by=list(measure)]
-  
-    #Filter out pvals that we are not significant
-    pval<-pval[!pval$pvalue == "",]
-    pval<-pval[as.numeric(pval$pvalue) <= 1,]
-    
-    #using sapply to generate significances for pval$pvalue using the cut function.
-    pval$pvalue<-sapply(as.numeric(pval$pvalue),function(x){as.character(cut(x,breaks=c(-Inf, 0.001, 0.01, pValueCutoff, Inf),label=c("***", "**", "*", "")))})
-    
-    #Update df$measure to change the measure names if the grouping_column has more than three classes
-    if(length(unique(as.character(meta_table[,grouping_column])))>2){
-      df$measure<-as.character(df$measure)
-      if(dim(pval)[1]>0){
-        for(i in seq(1:dim(pval)[1])){
-          df[df$measure==as.character(pval[i,measure]),"measure"]=paste(as.character(pval[i,measure]),as.character(pval[i,pvalue]))
-        }
-      }
-      df$measure<-as.factor(df$measure)
-    }
-    #Get all possible pairwise combination of values in the grouping_column
-    s<-combn(unique(as.character(df[,grouping_column])),2)
-    
-    #df_pw will store the pair-wise p-values
-    df_pw<-NULL
-    for(k in unique(as.character(df$measure))){
-      #We need to calculate the coordinate to draw pair-wise significance lines
-      #for this we calculate bas as the maximum value
-      bas<-max(df[(df$measure==k),"value"])
-      #Calculate increments as 10% of the maximum values
-      inc<-0.1*bas
-      #Give an initial increment
-      bas<-bas+inc
-      for(l in 1:dim(s)[2]){
-        #Do a pair-wise anova
-        tmp<-c(k,s[1,l],s[2,l],bas,paste(sprintf("%.2g",tryCatch(summary(aov(as.formula(paste("value ~",grouping_column)),data=df[(df$measure==k) & (df[,grouping_column]==s[1,l] | df[,grouping_column]==s[2,l]),] ))[[1]][["Pr(>F)"]][1],error=function(e) NULL)),"",sep=""))
-        #Ignore if anova fails
-        if(!is.na(as.numeric(tmp[length(tmp)]))){
-          #Only retain those pairs where the p-values are significant
-          if(as.numeric(tmp[length(tmp)])<pValueCutoff){
-            if(is.null(df_pw)){df_pw<-tmp}else{df_pw<-rbind(df_pw,tmp)}
-            #Generate the next position
-            bas<-bas+inc
-          }
-        }
-      }
-    }
-    if(!is.null(df_pw)){
-      #df_pw2 <- data.frame() 
-      df_pw <- rbind(df_pw )
-      colnames(df_pw)<-c("measure","from","to","y","p")
-      
-      
-    }
-    out <- list("df_pw"=df_pw, "df"=df)
-    return(out)
-  }
-
-#Generate alpha diversity table front-end (with Standard Error)
+  #Generate alpha diversity table front-end (with Standard Error)
   alpha.div.tab.se <- function(physeq, method, grouping_column, add.se){
     #calculate alpha diversity indices
     alpha <- alpha.div.tab(physeq = physeq, 
@@ -320,4 +196,189 @@ source("tools_functions.R")
     rownames(alpha.tab) <- alpha.mean.sp$Factor
     
     return(alpha.tab)
+  }
+  
+  #Perform statistical ANOVA test
+  # Modified from alpha diversity in microbiomSeq package (umerijaz/microbiomeSeq)  
+  TukeyHSD_Test <- function(df, meta_table, grouping_column, pValueCutoff){
+    #temp <- data.frame(df, meta_table[, grouping_column])
+    temp <- data.frame(df)
+    #colnames(temp) <- c(colnames(df), "group")
+    colnames(temp)[4] <- "group"
+    dt <- data.table::data.table(temp)
+    
+    # p-value annotation using overall ANOVA
+    pval <- dt[, list(
+      pvalue = sprintf("%.2g", tryCatch(
+        summary(aov(value ~ group))[[1]][["Pr(>F)"]][1],
+        error = function(e) NULL
+      ))
+    ), by = list(measure)]
+    
+    # Filter out nonsignificant or NA p-values
+    pval <- pval[!pval$pvalue == "", ]
+    pval <- pval[as.numeric(pval$pvalue) <= 1, ]
+    
+    # Add significance symbols
+    pval$pvalue <- sapply(as.numeric(pval$pvalue), function(x) {
+      as.character(cut(
+        x,
+        breaks = c(-Inf, 0.001, 0.01, pValueCutoff, Inf),
+        labels = c("***", "**", "*", "")
+      ))
+    })
+    
+    # Update df$measure with significance if grouping_column has >2 levels
+    if (length(unique(as.character(meta_table[, grouping_column]))) > 2) {
+      df$measure <- as.character(df$measure)
+      if (nrow(pval) > 0) {
+        for (i in seq_len(nrow(pval))) {
+          df[df$measure == as.character(pval[i, measure]), "measure"] <- paste0(
+            as.character(pval[i, measure]), as.character(pval[i, pvalue])
+          )
+        }
+      }
+      df$measure <- as.factor(df$measure)
+    }
+    
+    # Run TukeyHSD and extract pairwise comparisons + group letters
+    df_pw <- NULL
+    group_letters_list <- list()
+    
+    for (k in unique(as.character(df$measure))) {
+      sub_df <- df[df$measure == k, ]
+      sub_df$group <- as.factor(sub_df[, grouping_column])
+      aov_res <- tryCatch(aov(value ~ group, data = sub_df), error = function(e) NULL)
+      
+      if (!is.null(aov_res)) {
+        tukey_res <- tryCatch(TukeyHSD(aov_res), error = function(e) NULL)
+        
+        if (!is.null(tukey_res)) {
+          # Store compact letter display for this measure
+          cld <- tryCatch(multcompView::multcompLetters4(aov_res, tukey_res), error = function(e) NULL)
+          if (!is.null(cld)) {
+            letters_df <- data.frame(
+              group = names(cld$group$Letters),
+              letters = cld$group$Letters,
+              measure = k
+            )
+            group_letters_list[[k]] <- letters_df
+          }
+          
+          # Significant pairwise differences
+          tukey_table <- as.data.frame(tukey_res$group)
+          tukey_table$comparison <- rownames(tukey_table)
+          tukey_table$measure <- k
+          tukey_table <- tukey_table[tukey_table$`p adj` < pValueCutoff, ]
+          
+          if (nrow(tukey_table) > 0) {
+            base_y <- max(sub_df$value)
+            inc <- 0.1 * base_y
+            y_coords <- seq(base_y + inc, by = inc, length.out = nrow(tukey_table))
+            
+            split_comparisons <- do.call(rbind, strsplit(tukey_table$comparison, "-"))
+            df_pw_temp <- data.frame(
+              measure = tukey_table$measure,
+              from = split_comparisons[, 1],
+              to = split_comparisons[, 2],
+              y = y_coords,
+              p = sprintf("%.2g", tukey_table$`p adj`)
+            )
+            df_pw <- rbind(df_pw, df_pw_temp)
+          }
+        }
+      }
+    }
+    
+    # Combine all letter data frames into one
+    group_letters_df <- do.call(rbind, group_letters_list)
+    
+    # Final output
+    out <- list("df_pw" = df_pw, "df" = df, "group_letters" = group_letters_df)
+    return(out)
+  }
+  
+  
+  #Generate alpha diversity boxplot
+  # Modified from alpha diversity in microbiomSeq package (umerijaz/microbiomeSeq)  
+  alpha.div.plot <- function(physeq, method, grouping_column, pValueCutoff=0.05, neword=NULL, changefac=F, sig_display="none"){
+    #enforce orientation
+    if(taxa_are_rows(physeq)){
+      physeq <- t(physeq)
+    }
+    abund_table <- otu_table(physeq)
+    meta_table <- sample_data(physeq)
+    
+    #get diversity measure using selected methods
+    div.df <- alpha_div(physeq, method)
+    
+    #Exclude Goods coverage from the plot
+    div.df <- subset(div.df, measure!="Goods Coverage")
+    
+    
+    #=add grouping information to alpha diversity measures
+    df<-data.frame(div.df, (meta_table[,grouping_column])[as.character(div.df$sample),])
+    
+    #change the facto's order of the targeted column
+    if (changefac == T){
+      df <- order.levels.df(df = df,factor = grouping_column, orderd_factor1 = neword, changefac = changefac)
+    }
+    
+    #perform anova of diversity measure between groups if the group contain more than 1 element
+    df_pw <-NULL
+    group_letters <- NULL
+    if(length(unique(as.character(meta_table[[grouping_column]])))>1){
+      anova_Tukey_res <- TukeyHSD_Test(df,meta_table,grouping_column,pValueCutoff)
+      df_pw <- data.frame(anova_Tukey_res$df_pw) #get pairwise p-values
+      #df_pw$measure <- factor(df_pw$measure, levels = c("Richness", "Simpson", "Shannon", "Pielou's evenness", "PD"))
+      group_letters <- data.frame(anova_Tukey_res$group_letters) #Get TukeyHSD significant letters
+    }
+    
+    #Order indices
+    #df$measure <- factor(df$measure, levels = c("Richness", "Simpson", "Shannon", "Pielou's evenness", "PD"))
+    
+    #Draw the boxplots
+    p <- ggplot(aes_string(x=grouping_column,y="value",color=grouping_column),data=df)+
+      geom_boxplot()+
+      geom_jitter(position = position_jitter(height = 0, width=0))+
+      theme_bw()+
+      facet_wrap(~ measure, scales="free",nrow=1)+
+      ylab("Observed Values")+
+      theme(strip.background = element_rect(fill = "white"), panel.spacing = unit(0.005, "cm", data = NULL))+
+      xlab("")
+    
+    
+    #This loop will generate the lines and significance
+    if(!is.null(df_pw) && sig_display == "lines"){ #this only happens when we have significant pairwise anova results
+      df[,grouping_column] <- as.factor(df[,grouping_column])
+      for(i in 1:dim(df_pw)[1]){
+        p <- p + geom_path(inherit.aes=F,aes(x,y),data = data.frame(x = c(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"])),which(levels(df[,grouping_column])==as.character(df_pw[i,"to"]))), y = c(as.numeric(as.character(df_pw[i,"y"])),as.numeric(as.character(df_pw[i,"y"]))), measure=c(as.character(df_pw[i,"measure"]),as.character(df_pw[i,"measure"]))), color="black",lineend = "butt",arrow = arrow(angle = 90, ends = "both", length = unit(0.1, "inches")))
+        p <- p + geom_text(inherit.aes=F,aes(x=x,y=y,label=label),data=data.frame(x=(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"]))+which(levels(df[,grouping_column])==as.character(df_pw[i,"to"])))/2,y=as.numeric(as.character(df_pw[i,"y"])),measure=as.character(df_pw[i,"measure"]),label=as.character(cut(as.numeric(as.character(df_pw[i,"p"])),breaks=c(-Inf, 0.001, 0.01, 0.05, Inf),label=c("***", "**", "*", "")))))
+        #p<-p + geom_text(inherit.aes=F,aes(x=x,y=y,label=p.value),data=data.frame(x=(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"]))+which(levels(df[,grouping_column])==as.character(df_pw[i,"to"])))/2,y=(as.numeric(as.character(df_pw[i,"y"])))+((as.numeric(as.character(df_pw[i,"y"])))*0.025),measure=as.character(df_pw[i,"measure"]),p.value=as.character(df_pw[i,"p"])))
+        
+      }
+    }
+    
+    if(!is.null(group_letters) && sig_display == "letters"){ #this only happens when we have significant pairwise anova results
+      # Calculate y-position: max value per group & measure
+      colnames(df)[4] <- "group"
+      label_positions <- aggregate(value ~ group + measure, data = df, FUN = max)
+      colnames(label_positions)[3] <- "y"
+      
+      # Merge with group_letters_df to get both letter and y-position
+      group_letters_df_pos <- merge(group_letters, label_positions, by = c("group", "measure"))
+      
+      # Add a bit of vertical padding above the box
+      group_letters_df_pos$y <- group_letters_df_pos$y * 1.05
+      
+      p <- p +  geom_text(
+        data = group_letters_df_pos,
+        aes(x = group, y = y, label = letters),
+        inherit.aes = FALSE,
+        vjust = 0,
+        size = 5
+      )
+    }
+    
+    return(p)
   }
